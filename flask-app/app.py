1c0bf5abd6a88ecc114b89ba115f4ecb14855324
Ubuntu <ubuntu@ip-172-31-0-18.eu-west-1.compute.internal>
2023-04-15 12:49:00 +0000

    changed app.py file

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'It works! You deployed your app on an ECS cluster!'
 
 if __name__ == '__main__':
     app.run(debug=True)
